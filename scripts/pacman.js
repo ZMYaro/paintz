f8c1339621c2ac4dffca4a55a790ed7f8a1bfbbe
Zachary Yaro <zmyaro@gmail.com>
2014-10-16 12:48:16 -0400

    Animate Pac-Man's mouth

--- scripts/pacman.js
+++ scripts/pacman.js
@@ -9,13 +9,24 @@ function PacMan(canvas, x, y) {
 	this.cxt = canvas.getContext('2d');
 	this.x = x;
 	this.y = y;
-	this.heading = 90; // 0 = up
+	this.heading = PacMan.HEADINGS.RIGHT;
+	
+	this._mouthSize = 0;
+	this._mouthOpening = true;
 	
 	this._initListeners();
 }
 // Constants.
-PacMan.SPEED = 2;
 PacMan.RADIUS = 30;
+PacMan.SPEED = 2;
+PacMan.MOUTH_SPEED = Math.PI / 16;
+PacMan.MAX_MOUTH_SIZE = Math.PI / 4;
+PacMan.HEADINGS = {
+	RIGHT: 0,
+	DOWN: 0.5 * Math.PI,
+	LEFT: Math.PI,
+	UP: 1.5 * Math.PI
+};
 PacMan.KEYS = {
 	LEFT: [37, 65], // Left, A
 	UP: [38, 44, 87], // Up, comma, W
@@ -31,16 +42,16 @@ PacMan.prototype._initListeners = function () {
 	window.addEventListener('keydown', (function (e) {
 		if (PacMan.KEYS.UP.indexOf(e.keyCode) !== -1) {
 			e.preventDefault();
-			this.heading = 0;
+			this.heading = PacMan.HEADINGS.UP;
 		} else if (PacMan.KEYS.RIGHT.indexOf(e.keyCode) !== -1) {
 			e.preventDefault();
-			this.heading = 90;
+			this.heading = PacMan.HEADINGS.RIGHT;
 		} else if (PacMan.KEYS.DOWN.indexOf(e.keyCode) !== -1) {
 			e.preventDefault();
-			this.heading = 180;
+			this.heading = PacMan.HEADINGS.DOWN;
 		} else if (PacMan.KEYS.LEFT.indexOf(e.keyCode) !== -1) {
 			e.preventDefault();
-			this.heading = 270;
+			this.heading = PacMan.HEADINGS.LEFT;
 		}
 	}).bind(this), false);
 };
@@ -49,25 +60,45 @@ PacMan.prototype.update = function () {
 	// “Eat” everything under Pac-Man.
 	this.cxt.fillStyle = fillColor;
 	this.cxt.beginPath();
-	this.cxt.arc(this.x, this.y, PacMan.RADIUS, 0, 2 * Math.PI);
+	this.cxt.arc(this.x, this.y, PacMan.RADIUS, 0.5 * Math.PI + this.heading, 1.5 * Math.PI + this.heading);
+	this.cxt.closePath();
+	this.cxt.fill();
+	this.cxt.beginPath();
+	this.cxt.moveTo(this.x, this.y);
+	this.cxt.arc(this.x, this.y, PacMan.RADIUS, this._mouthSize + this.heading, 0.5 * Math.PI + this.heading);
+	this.cxt.closePath();
+	this.cxt.fill();
+	this.cxt.beginPath();
+	this.cxt.moveTo(this.x, this.y);
+	this.cxt.arc(this.x, this.y, PacMan.RADIUS, 1.5 * Math.PI + this.heading, 2 * Math.PI - this._mouthSize + this.heading);
 	this.cxt.closePath();
 	this.cxt.fill();
 	
 	// Move Pac-Man.
 	switch (this.heading) {
-		case 0:
+		case PacMan.HEADINGS.UP:
 			this.y -= PacMan.SPEED;
 			break;
-		case 90:
+		case PacMan.HEADINGS.RIGHT:
 			this.x += PacMan.SPEED;
 			break;
-		case 180:
+		case PacMan.HEADINGS.DOWN:
 			this.y += PacMan.SPEED;
 			break;
-		case 270:
+		case PacMan.HEADINGS.LEFT:
 			this.x -= PacMan.SPEED;
 			break;
 	}
+	// Animate Pac-Man's mouth.
+	this._mouthSize += PacMan.MOUTH_SPEED * (this._mouthOpening ? 1 : -1);
+	if (this._mouthOpening && this._mouthSize > PacMan.MAX_MOUTH_SIZE) {
+		this._mouthSize = PacMan.MAX_MOUTH_SIZE;
+		this._mouthOpening = false;
+	} else if (!this._mouthOpening && this._mouthSize < 0) {
+		this._mouthSize = 0;
+		this._mouthOpening = true;
+	}
+		
 	// Screen wrap.
 	if (this.x < -(PacMan.RADIUS * 2)) {
 		this.x = canvas.width + (PacMan.RADIUS * 2);
@@ -80,10 +111,19 @@ PacMan.prototype.update = function () {
 	}
 	
 	// Draw Pac-Man.
-	// TODO: Animate Pac-Man.
 	this.cxt.fillStyle = 'yellow';
 	this.cxt.beginPath();
-	this.cxt.arc(this.x, this.y, PacMan.RADIUS - 1, 0, 2 * Math.PI);
+	this.cxt.arc(this.x, this.y, PacMan.RADIUS - 1, 0.5 * Math.PI + this.heading, 1.5 * Math.PI + this.heading);
+	this.cxt.closePath();
+	this.cxt.fill();
+	this.cxt.beginPath();
+	this.cxt.moveTo(this.x, this.y);
+	this.cxt.arc(this.x, this.y, PacMan.RADIUS - 1, this._mouthSize + this.heading, 0.5 * Math.PI + this.heading);
+	this.cxt.closePath();
+	this.cxt.fill();
+	this.cxt.beginPath();
+	this.cxt.moveTo(this.x, this.y);
+	this.cxt.arc(this.x, this.y, PacMan.RADIUS - 1, 1.5 * Math.PI + this.heading, 2 * Math.PI - this._mouthSize + this.heading);
 	this.cxt.closePath();
 	this.cxt.fill();
 	
