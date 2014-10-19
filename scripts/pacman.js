0afd62bb3fdaa04a261bfa973b5ed2e61919dd8d
Zachary Yaro <zmyaro@gmail.com>
2014-10-19 02:52:08 -0400

    Organized Pac-Man private functions

--- scripts/pacman.js
+++ scripts/pacman.js
@@ -7,18 +7,18 @@
 function PacMan(canvas, x, y) {
 	this._started = false;
 	
-	this.canvas = canvas;
-	this.cxt = canvas.getContext('2d');
+	this._canvas = canvas;
+	this._cxt = canvas.getContext('2d');
 	this.x = x || Math.floor(canvas.width * 0.2);
 	this.y = y || Math.floor(canvas.height * 0.2);
 	this.heading = PacMan.HEADINGS.RIGHT;
 	
-	this.startSound = document.getElementById('pacManStartSound');
+	this._startSound = document.getElementById('pacManStartSound');
 	
 	this._mouthSize = 0;
 	this._mouthOpening = true;
 	
-	this.update = this._update.bind(this);
+	this._boundUpdate = this._update.bind(this);
 	
 	this._initListeners();
 }
@@ -40,7 +40,70 @@ PacMan.KEYS = {
 	RIGHT: [39, 68, 69], // Right, D, E
 	DOWN: [40, 79, 83] // Down, O, S
 };
-	
+
+
+/**
+ * Play the starting sound and start Pac-Man after it plays.
+ */
+PacMan.prototype.start = function () {
+	// Do not start this Pac-Man if it has already started.
+	if (this._started) {
+		return;
+	}
+	// Draw Pac-Man, but do not start him moving yet.
+	this._update();
+	// Play the start sound.
+	if (this._startSound && this._startSound.play) {
+		this._startSound.play();
+	}
+	// NOW set Pac-Man to have started.
+	this._started = true;
+	// Start moving after the sound finishes. 
+	setTimeout(this._boundUpdate, PacMan.START_SOUND_LENGTH);
+};
+
+/**
+ * Draw Pac-Man to the canvas.
+ */
+PacMan.prototype._draw = function () {
+	this._cxt.fillStyle = 'yellow';
+	this._cxt.beginPath();
+	this._cxt.arc(this.x, this.y, PacMan.RADIUS - 1, 0.5 * Math.PI + this.heading, 1.5 * Math.PI + this.heading);
+	this._cxt.closePath();
+	this._cxt.fill();
+	this._cxt.beginPath();
+	this._cxt.moveTo(this.x, this.y);
+	this._cxt.arc(this.x, this.y, PacMan.RADIUS - 1, this._mouthSize + this.heading, 0.5 * Math.PI + this.heading);
+	this._cxt.closePath();
+	this._cxt.fill();
+	this._cxt.beginPath();
+	this._cxt.moveTo(this.x, this.y);
+	this._cxt.arc(this.x, this.y, PacMan.RADIUS - 1, 1.5 * Math.PI + this.heading, 2 * Math.PI - this._mouthSize + this.heading);
+	this._cxt.closePath();
+	this._cxt.fill();
+};
+
+/**
+ * Cover Pac-Man with the current fill color, effectively erasing him
+ * and “eating” everything under him.
+ */
+PacMan.prototype._erase = function () {
+	this._cxt.fillStyle = fillColor;
+	this._cxt.beginPath();
+	this._cxt.arc(this.x, this.y, PacMan.RADIUS, 0.5 * Math.PI + this.heading, 1.5 * Math.PI + this.heading);
+	this._cxt.closePath();
+	this._cxt.fill();
+	this._cxt.beginPath();
+	this._cxt.moveTo(this.x, this.y);
+	this._cxt.arc(this.x, this.y, PacMan.RADIUS, this._mouthSize + this.heading, 0.5 * Math.PI + this.heading);
+	this._cxt.closePath();
+	this._cxt.fill();
+	this._cxt.beginPath();
+	this._cxt.moveTo(this.x, this.y);
+	this._cxt.arc(this.x, this.y, PacMan.RADIUS, 1.5 * Math.PI + this.heading, 2 * Math.PI - this._mouthSize + this.heading);
+	this._cxt.closePath();
+	this._cxt.fill();
+};
 
 /**
  * Initialize key event listeners for Pac-Man.
@@ -63,48 +126,14 @@ PacMan.prototype._initListeners = function () {
 	}).bind(this), false);
 };
 
-/**
- * Play the starting sound and start Pac-Man after it plays.
- */
-PacMan.prototype.start = function () {
-	// Do not start this Pac-Man if it has already started.
-	if (this._started) {
-		return;
-	}
-	// Draw Pac-Man, but do not start him moving yet.
-	this._update();
-	// Play the start sound.
-	if (this.startSound && this.startSound.play) {
-		this.startSound.play();
-	}
-	// NOW set Pac-Man to have started.
-	this._started = true;
-	// Start moving after the sound finishes. 
-	setTimeout(this.update, PacMan.START_SOUND_LENGTH);
-};
-
 PacMan.prototype._update = function () {
 	// If this Pac-Man is supposed to be running, loop.
 	if (this._started) {
-		Utils.raf(this.update);
+		Utils.raf(this._boundUpdate);
 	}
 	
 	// “Eat” everything under Pac-Man.
-	this.cxt.fillStyle = fillColor;
-	this.cxt.beginPath();
-	this.cxt.arc(this.x, this.y, PacMan.RADIUS, 0.5 * Math.PI + this.heading, 1.5 * Math.PI + this.heading);
-	this.cxt.closePath();
-	this.cxt.fill();
-	this.cxt.beginPath();
-	this.cxt.moveTo(this.x, this.y);
-	this.cxt.arc(this.x, this.y, PacMan.RADIUS, this._mouthSize + this.heading, 0.5 * Math.PI + this.heading);
-	this.cxt.closePath();
-	this.cxt.fill();
-	this.cxt.beginPath();
-	this.cxt.moveTo(this.x, this.y);
-	this.cxt.arc(this.x, this.y, PacMan.RADIUS, 1.5 * Math.PI + this.heading, 2 * Math.PI - this._mouthSize + this.heading);
-	this.cxt.closePath();
-	this.cxt.fill();
+	this._erase();
 	
 	// Move Pac-Man.
 	switch (this.heading) {
@@ -143,19 +172,5 @@ PacMan.prototype._update = function () {
 	}
 	
 	// Draw Pac-Man.
-	this.cxt.fillStyle = 'yellow';
-	this.cxt.beginPath();
-	this.cxt.arc(this.x, this.y, PacMan.RADIUS - 1, 0.5 * Math.PI + this.heading, 1.5 * Math.PI + this.heading);
-	this.cxt.closePath();
-	this.cxt.fill();
-	this.cxt.beginPath();
-	this.cxt.moveTo(this.x, this.y);
-	this.cxt.arc(this.x, this.y, PacMan.RADIUS - 1, this._mouthSize + this.heading, 0.5 * Math.PI + this.heading);
-	this.cxt.closePath();
-	this.cxt.fill();
-	this.cxt.beginPath();
-	this.cxt.moveTo(this.x, this.y);
-	this.cxt.arc(this.x, this.y, PacMan.RADIUS - 1, 1.5 * Math.PI + this.heading, 2 * Math.PI - this._mouthSize + this.heading);
-	this.cxt.closePath();
-	this.cxt.fill();
+	this._draw();
 };
