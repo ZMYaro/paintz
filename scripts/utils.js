991102412ab78672a9a981b7f30fb35746e40a82
Zachary Yaro <zmyaro@gmail.com>
2014-10-20 13:08:46 -0400

    Add dialog box framework

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,4 +1,6 @@
 var Utils = {
+	DIALOG_TRANSITION_DURATION: 200, // In milliseconds.
+	
 	/**
 	 * Get the x-coordinate of a click within the canvas.
 	 * @param {Number} pageX - The x-coordinate relative to the page
@@ -13,6 +15,36 @@ var Utils = {
 	 */
 	getCanvasY: function (pageY) {
 		return pageY - preCanvas.offsetTop;
+	},
+	
+	/**
+	 * Add dialog box functions to an element
+	 * @param {HTMLElement} element - The dialog's HTML element
+	 */
+	makeDialog: function (element) {
+		element.open = function () {
+			element.classList.add('visible');
+			setTimeout(function () {
+				element.classList.add('open');
+				// Focus the first form element in the dialog.
+				element.querySelectorAll('button, input, select, textarea')[0].focus();
+			}, 1);
+		};
+		element.close = function (e) {
+			if (e && e.preventDefault) {
+				e.preventDefault();
+			}
+			element.classList.remove('open');
+			// After the closing animation has completed, hide the dialog box element completely.
+			setTimeout(function () {
+				element.classList.remove('visible');
+			}, Utils.DIALOG_TRANSITION_DURATION);
+		};
+		if (element instanceof HTMLFormElement) {
+			element.onsubmit = function (e) {
+				e.target.close();
+			};
+		}
 	},
 	
 	/**
