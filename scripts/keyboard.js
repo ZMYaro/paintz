723674d6f26e7424c24ef1b3e39260415778738c
Zachary Yaro <zmyaro@gmail.com>
2015-01-09 16:18:36 -0500

    Add keyboard shortcuts for open and save

--- scripts/keyboard.js
+++ scripts/keyboard.js
@@ -1,13 +1,31 @@
 var keyManager = {
 	_keyPressListener: function (e) {
 		switch (e.keyCode) {
+			case 79: // O
+				if (e.ctrlKey) {
+					e.preventDefault();
+					// Ctrl+O => Open
+					document.getElementById('upload').click();
+				}
+				break;
+			case 83: // S
+				if (e.ctrlKey) {
+					e.preventDefault();
+					// Ctrl+S => Save
+					downloadImage();
+				}
+				break;
 			case 89: // Y
 				if (e.ctrlKey) {
+					e.preventDefault();
+					// Ctrl+Y => Redo
 					undoStack.redo();
 				}
 				break;
 			case 90: // Z
 				if (e.ctrlKey) {
+					e.preventDefault();
+					// Ctrl+Z => Undo
 					undoStack.undo();
 				}
 				break;
