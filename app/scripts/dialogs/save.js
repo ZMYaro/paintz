5e07ce79cf997595b18fba3037cc3ff504677238
Zachary Yaro <zmyaro@zmyaro.com>
2019-05-24 01:35:35 -0400

    Show progress spinner in save dialog while blob URL is generated

--- app/scripts/dialogs/save.js
+++ app/scripts/dialogs/save.js
@@ -7,6 +7,7 @@
  */
 function SaveDialog(trigger) {
 	Dialog.call(this, 'save', trigger);
+	this._progressSpinner;
 	this._downloadLink;
 	this._boundSetDownloadURL = this._setDownloadURL.bind(this);
 }
@@ -31,7 +32,10 @@ SaveDialog.prototype._setUp = function (contents) {
 		this._element.fileType.onchange =
 		this._element.fileType.oninput = this._updateFileExtension.bind(this);
 	
+	this._progressSpinner = this._element.querySelector('progress');
+	
 	this._downloadLink = this._element.querySelector('#downloadLink');
+	this._downloadLink.style.display = 'none';
 	this._downloadLink.onclick = (function () {
 		// Web app cannot confirm the user went through with the download, but assume xe did.
 		document.title = this._downloadLink.download + ' - PaintZ';
@@ -51,7 +55,7 @@ SaveDialog.prototype.open = function () {
 	Dialog.prototype.open.call(this);
 	
 	// Export the canvas content to an image to be saved.
-	canvas.toBlob(this._boundSetDownloadURL, this._downloadLink.type || 'image/png');
+	this._createDownloadURL();
 };
 
 /**
@@ -66,12 +70,27 @@ SaveDialog.prototype.close = function (e) {
 
 /**
  * @private
+ * Create a new blob URL, and set it as the download URL when done.
+ */
+SaveDialog.prototype._createDownloadURL = function () {
+	this._downloadLink.style.display = 'none';
+	this._progressSpinner.style.removeProperty('display');
+	
+	canvas.toBlob(this._boundSetDownloadURL, this._downloadLink.type || 'image/png');
+};
+
+/**
+ * @private
  * Set the download URL using the blob from the canvas.
  * @param {Blob} blob - The image blob from the canvas
  */
 SaveDialog.prototype._setDownloadURL = function (blob) {
+	URL.revokeObjectURL(this._downloadLink.href);
 	var url = URL.createObjectURL(blob);
 	this._downloadLink.href = url;
+	
+	this._downloadLink.style.removeProperty('display');
+	this._progressSpinner.style.display = 'none';
 };
 
 /**
@@ -83,8 +102,6 @@ SaveDialog.prototype._updateFileExtension = function () {
 	var newName = fixExtension(this._element.fileName.value, this._element.fileType.value);
 	this._element.fileName.value = newName;
 	this._downloadLink.download = newName;
-	this._downloadLink.href = canvas.toDataURL(this._element.fileType.value);
-	
-	// Update file type.
 	this._downloadLink.type = this._element.fileType.value;
+	this._createDownloadURL();
 };
