8c7205a9afb0c390702e50da89e03f1f76568ae2
Zachary Yaro <zmyaro@zmyaro.com>
2019-07-21 01:54:59 -0400

    Fix the selection outline not drawing after doing a Select all

--- app/scripts/tools/selection.js
+++ app/scripts/tools/selection.js
@@ -288,6 +288,7 @@ SelectionTool.prototype.selectAll = function (width, height) {
 	this.start({x: 0, y: 0});
 	this.move({x: width, y: height});
 	this.end({x: width, y: height});
+	this._updateSelectionOutline();
 };
 
 /**
