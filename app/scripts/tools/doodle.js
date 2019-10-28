c74cf76b533095933fe104722dfe9728f80fb4f2
Zachary Yaro <zmyaro@zmyaro.com>
2019-10-28 01:21:22 -0400

    Add crosshairs to brush cursors that appear smaller than 16px in diameter

--- app/scripts/tools/doodle.js
+++ app/scripts/tools/doodle.js
@@ -14,6 +14,12 @@ DoodleTool.prototype.constructor = DoodleTool;
 
 /** {Number} The maximum allowed width/height for the cursor, in pixels */
 DoodleTool.MAX_CURSOR_SIZE = 128;
+/** {Number} The maximum brush size at which crosshairs are needed */
+DoodleTool.MAX_CROSSHAIR_SIZE = 16
+/** {Number} The length of the crosshair lines, in pixels */
+DoodleTool.CROSSHAIR_LENGTH = 12;
+/** {Number} The offset of the crosshairs from the outside of the brush, in pixels */
+DoodleTool.CROSSHAIR_OFFSET = 4;
 
 /**
  * @private
@@ -116,27 +122,65 @@ DoodleTool.prototype.update = function () {
  * @returns {String}
  */
 DoodleTool.getCursorCSS = function () {
-	var size = (parseInt(settings.get('lineWidth')) + 1) * zoomManager.level;
-	
-	// Set the cursor size, capped at 128px.
-	cursorCanvas.width = cursorCanvas.height = Math.min(DoodleTool.MAX_CURSOR_SIZE, size);
+	var size = (parseInt(settings.get('lineWidth')) + 1) * zoomManager.level,
+		halfCanvasSize = size / 2;
 	
 	// Switch to a crosshair when the cursor gets too big.
 	if (size > DoodleTool.MAX_CURSOR_SIZE * Math.sqrt(2)) {
 		return 'crosshair';
 	}
 	
-	cursorCxt.save();
+	// Set the cursor size.
+	cursorCanvas.width =
+		cursorCanvas.height = size;
 	
+	cursorCxt.save();
 	cursorCxt.lineWidth = 1;
+	
+	// Draw crosshairs around the circle if it is small enough.
+	if (size < DoodleTool.MAX_CROSSHAIR_SIZE) {
+		// Increase the canvas size to make room for the crosshairs.
+		cursorCanvas.width =
+			cursorCanvas.height = cursorCanvas.width + DoodleTool.CROSSHAIR_OFFSET + DoodleTool.CROSSHAIR_LENGTH;
+		halfCanvasSize = cursorCanvas.width / 2;
+		
+		console.log('drawing crosshair');
+		// Draw the crosshairs.
+		cursorCxt.translate(halfCanvasSize, halfCanvasSize);
+		cursorCxt.beginPath();
+		// Right
+		cursorCxt.moveTo( size / 2 + DoodleTool.CROSSHAIR_OFFSET,                               0);
+		cursorCxt.lineTo( size / 2 + DoodleTool.CROSSHAIR_OFFSET + DoodleTool.CROSSHAIR_LENGTH, 0);
+		// Left
+		cursorCxt.moveTo(-size / 2 - DoodleTool.CROSSHAIR_OFFSET,                               0);
+		cursorCxt.lineTo(-size / 2 - DoodleTool.CROSSHAIR_OFFSET - DoodleTool.CROSSHAIR_LENGTH, 0);
+		// Top
+		cursorCxt.moveTo(0,  size / 2 + DoodleTool.CROSSHAIR_OFFSET);
+		cursorCxt.lineTo(0,  size / 2 + DoodleTool.CROSSHAIR_OFFSET + DoodleTool.CROSSHAIR_LENGTH);
+		// Bottom
+		cursorCxt.moveTo(0, -size / 2 - DoodleTool.CROSSHAIR_OFFSET);
+		cursorCxt.lineTo(0, -size / 2 - DoodleTool.CROSSHAIR_OFFSET - DoodleTool.CROSSHAIR_LENGTH);
+		cursorCxt.translate(-halfCanvasSize, -halfCanvasSize);
+		
+		cursorCxt.setLineDash([]);
+		cursorCxt.strokeStyle = 'white';
+		cursorCxt.stroke();
+		cursorCxt.setLineDash([1, 1]);
+		cursorCxt.strokeStyle = 'black';
+		cursorCxt.stroke();
+		
+		cursorCxt.closePath();
+	}
+	
 	cursorCxt.beginPath();
 	cursorCxt.arc(
-		cursorCanvas.width / 2, cursorCanvas.height / 2,
+		halfCanvasSize, halfCanvasSize,
 		size / 2,
 		0, Math.TAU, false
 	);
 	cursorCxt.closePath();
 	
+	cursorCxt.setLineDash([]);
 	cursorCxt.strokeStyle = 'white';
 	cursorCxt.stroke();
 	cursorCxt.setLineDash([2, 2]);
